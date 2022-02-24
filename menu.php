8080bebe7b9a04d46d4e0a5b3d2d042ca53c47af
shinsaku <01103709@kuronekoyamato.co.jp>
2022-02-24 20:28:28 +0900

    Merge branch 'main' of https://github.com/outsider2022/kintaikanri

--- menu.php
+++ menu.php
@@ -10,12 +10,14 @@
 
 <div class="menu">
             <a href="workreg.php">出社登録</a>
-            <a href="workdetails.php">登録一覧</a>
             <br><br>
-            <a href="workplace.php">出社先管理</a>
+            <a href="workdetails.php">登録内容確認</a>
+            <br><br>
+            <a href="workplace.php">出勤先管理</a>
             <a href="department.php">部門管理</a>
-            <a href="employee.php">社員管理</a>
+            <a href="employee.php">社員マスタ管理</a>
             <a href="user.php">ユーザー管理</a>
+            <a href="????.php">管理者確認ページ(一覧表示・データ修正)</a>
 </div>
 
 </body>
