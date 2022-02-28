dcfe6192f12f12ffded3387c33e2c396bbc805ad
k4n4 <kshimamu@kuronekoyamato.co.jp>
2022-02-28 19:50:35 +0900

    20220228打ち合わせ中に変更したものたちを上げます

--- login1/index.php
+++ login1/index.php
@@ -1,14 +1,15 @@
 <link rel="stylesheet" href="style.css/style.css">
 
 <html>
-<form name="login_form">
+<!-- <form name="login_form"> -->
+<form method="POST" action="login_act.php"> 
      <div class="login_form_top">
     <h1>ログイン画面</h1>
     <p>社員番号、パスワードをご入力の上、「ログイン」ボタンをクリックしてください。</p>
   </div>
   <div class="login_form_btm">
-    <input type="id" name="user_id" placeholder="社員番号を入力してください"><br>
-    <input type="password" name="password"placeholder="パスワードを入力してください">
+    <input type="text" name="user_id" placeholder="社員番号を入力してください"><br>
+    <input type="password" name="password" placeholder="パスワードを入力してください">
   </div>
   <button type="submit">ログイン</button>
 </form>
--- menu.php
+++ menu.php
@@ -2,45 +2,39 @@
 //セッションをスタート
 session_start();
 
-$user_id = $_SESSION['user_id'];
+// $lid = $_POST['lid'];
+// $lpw = $_POST['lpw'];
 
-//関数を呼び出して
+// 社員番号を直打ち
+//$lid = "00056125";
+
+// //関数を呼び出して
 require_once('funcs.php');
 
-//ログインチェック
-loginCheck();
+// //ログインチェック
+// loginCheck();
 
-//以下ログインユーザーのみ
-
+// //以下ログインユーザーのみ
 $pdo = db_conn();
+
+// SQL作成
+$stmt = $pdo->prepare("SELECT * FROM user ");
 
 //３．データ表示
 $view="";
 if($==false) {
   sql_error($stmt);
 }else{
-  while( $r = $stmt->fetch(PDO::FETCH_ASSOC)){ 
+  while( $r = $stmt->fetch(PDO::FETCH_ASSOC)){
     $view .= '<div class="menu">';
-    $view .= '<a href="workplace.php">出勤先管理</a>';
-    $view .= '<a href="department.php">部門管理</a>'
-    $view .= '<a href="employee.php">社員マスタ管理</a>';
-    $view .= '<a href="user.php">ユーザー管理</a>';
-    $view .= '<a href="administratorconfirm.php">管理者確認ページ(一覧表示・データ修正)</a>';
+    $view .= '<a href="workplace.php?empno=$lid">出勤先管理</a>';
+    $view .= '<a href="department.php?empno=$lid">部門管理</a>';
+    $view .= '<a href="employee.php?empno=$lid">社員マスタ管理</a>';
+    $view .= '<a href="user.php?empno=$lid">ユーザー管理</a>';
+    $view .= '<a href="administratorconfirm.php?empno=$lid">管理者確認ページ(一覧表示・データ修正)</a>';
   }
 }
 ?>
-<!-- <div class="menu">
-         <a href="workplace.php">出勤先管理</a>
-         <br><br>
-         <a href="department.php">部門管理</a>
-         <br><br>
-         <a href="employee.php">社員マスタ管理</a>
-         <br><br>
-         <a href="user.php">ユーザー管理</a>
-         <br><br>
-         <a href="administratorconfirm.php">管理者確認ページ(一覧表示・データ修正)</a> -->
-     </div>
-
 
 <!DOCTYPE html>
 <html lang="ja">
@@ -53,6 +47,9 @@ if($==false) {
 <!-- ここからヘッダー -->
 <body>
 <header>
+  <p>
+  <var></var>
+  </p>
     <h1 class="header-title">メインメニュー</h1>
     <nav class="nav">
       <ul class="menu-group">
@@ -69,9 +66,11 @@ if($==false) {
 <!-- ここからテキストリンク -->
 <div class="menu_box">
      <div class="menu">
-         <a href="workreg.php?empno=">出社登録</a>
+         <a href="workregchk.php?empno=00056125">出社登録</a>
          <br><br>
          <a href="workdetails.php?empno=">登録内容確認</a>
+         <br><br>
+         <a href="administratorconfirm.php">管理者確認ページ</a>
      </div>
 </div>
 
@@ -79,4 +78,3 @@ if($==false) {
 
 
 </html>
-
