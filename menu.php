126ab6beddb9049a243c7ef98af8cae1cd9c5781
shinsaku <01103709@kuronekoyamato.co.jp>
2022-03-01 19:02:33 +0900

    main

--- menu.php
+++ menu.php
@@ -1,10 +1,10 @@
 <?php
 //セッションをスタート
-// session_start();
+session_start();
 
 //ログインユーザーidとroleを取得
 $lid = $_SESSION['id'];
-$role = $_SESSION['ROLE']
+$role = $_SESSION['ROLE'];
 
 // テスト：idとroleを直打ち
 // $lid = "00056125";
@@ -15,27 +15,25 @@ $role = $_SESSION['ROLE']
 $view="";
 // roleが1：管理者画面も追加
 if ($role == 1) {
-  $view .= '<a href="workregchk.php?empno=$lid">出社登録</a>';
+  $view .= '<a href="workregchk.php?empno='.$lid.'">出社登録</a>';
   $view .= '<br><br>';
-  $view .= '<a href="workdetails.php?empno=$lid">登録内容確認</a>';
+  $view .= '<a href="worklist.php?empno='.$lid.'">登録内容確認</a>';
   $view .= '<br><br>';
-  $view .= '<a href="administratorconfirm.php?empno=$lid">管理者確認ページ</a>';;
+  $view .= '<a href="administratorconfirm.php">管理者確認ページ</a>';;
   $view .= '<br><br>';
-  $view .= '<a href="workplace.php?empno=$lid">出勤先管理</a>';
+  $view .= '<a href="workplace.php">出勤先管理</a>';
   $view .= '<br><br>';
-  $view .= '<a href="department.php?empno=$lid">部門管理</a>';
+  $view .= '<a href="department.php">部門管理</a>';
   $view .= '<br><br>';
-  $view .= '<a href="employee.php?empno=$lid">社員マスタ管理</a>';
+  $view .= '<a href="employee.php">社員マスタ管理</a>';
   $view .= '<br><br>';
-  $view .= '<a href="user.php?empno=$lid">ユーザー管理</a>';
-  $view .= '<br><br>';
-  $view .= '<a href="administratorconfirm.php?empno=$lid">管理者確認ページ</a>';
+  $view .= '<a href="administratorconfirm.php">管理者確認ページ</a>';
   $view .= '<br><br>';
 } else {
   // roleが1以外：出社登録、登録内容確認
-  $view .= '<a href="workregchk.php?empno=00056125">出社登録</a>';
+  $view .= '<a href="workregchk.php?empno='.$lid.'">出社登録</a>';
   $view .= '<br><br>';
-  $view .= '<a href="workdetails.php?empno=">登録内容確認</a>';
+  $view .= '<a href="worklist.php?empno='.$lid.'">登録内容確認</a>';
   $view .= '<br><br>'; 
 }
 
@@ -59,11 +57,8 @@ if ($role == 1) {
     <h1 class="header-title">メインメニュー</h1>
     <nav class="nav">
       <ul class="menu-group">
-        <li class="menu-item"><a href="#">項目1</a></li>
-        <li class="menu-item"><a href="#">項目2</a></li>
-        <li class="menu-item"><a href="#">項目3</a></li>
-        <li class="menu-item"><a href="#">項目4</a></li>
-        <li class="menu-item"><a href="#">項目5</a></li>
+        <li class="menu-item"><a href="logout.php">ログアウト</a></li>
+
       </ul>
     </nav>
   </header>
